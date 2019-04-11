a86bc87335be3164dca49faa866b821dbd41fa3b
ezganlu <louie.gan@ericsson.com>
2019-04-11 08:16:17 +0200

    Revert "Update go api and merge with other forks"
    This reverts commit 86d1257dcc3a0da1f36db924ae6ed89a5c0bc5c3.

--- kubetop.go
+++ kubetop.go
@@ -16,12 +16,9 @@ import (
 	"github.com/fatih/color"
 	"github.com/olekukonko/tablewriter"
 
-	//"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/clientcmd"
-	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 var (
@@ -30,7 +27,6 @@ var (
 	colorService    = color.New(color.FgBlue).SprintFunc()
 	colorDeployment = color.New(color.FgMagenta).SprintFunc()
 	colorFailed     = color.New(color.FgRed).SprintFunc()
-	colorWarning    = color.New(color.FgYellow).SprintFunc()
 
 	flagNamespace = flag.String("namespace", "", "filter resources by namespace")
 )
@@ -72,17 +68,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	nodes, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var nodeNames []string
-	for _, node := range nodes.Items {
-    nodeNames = append(nodeNames, node.ObjectMeta.Name)
-	}
-	lcpNodes := lcp(nodeNames)
-
 	var rows Rows
 	var ch chan Rows
 	for {
@@ -100,7 +85,7 @@ func main() {
 		go func() { defer wg.Done(); getNodes(ch, clientset) }()
 		go func() { defer wg.Done(); getServices(ch, clientset) }()
 		go func() { defer wg.Done(); getDeployments(ch, clientset) }()
-		go func() { defer wg.Done(); getPods(ch, clientset, lcpNodes) }()
+		go func() { defer wg.Done(); getPods(ch, clientset) }()
 		wg.Wait()
 		close(ch)
 
@@ -111,7 +96,6 @@ func main() {
 			"Namespace",
 			"Name",
 			"Status",
-			"Node",
 			"IPs",
 			"Age",
 		}, rows)
@@ -120,7 +104,7 @@ func main() {
 }
 
 func getNodes(ch chan Rows, clientset *kubernetes.Clientset) {
-	nodes, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
+	nodes, err := clientset.Core().Nodes().List(v1.ListOptions{})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -155,7 +139,6 @@ func getNodes(ch chan Rows, clientset *kubernetes.Clientset) {
 			colorNode(node.ObjectMeta.Namespace),
 			colorNode(node.ObjectMeta.Name),
 			colorNode(strings.Join(statuses, " ")),
-			colorNode(""), // Node
 			colorNode(strings.Join(addresses, " ")),
 			colorNode(shortHumanDuration(time.Since(node.CreationTimestamp.Time))),
 		})
@@ -164,7 +147,7 @@ func getNodes(ch chan Rows, clientset *kubernetes.Clientset) {
 }
 
 func getServices(ch chan Rows, clientset *kubernetes.Clientset) {
-	services, err := clientset.CoreV1().Services("").List(metav1.ListOptions{})
+	services, err := clientset.Core().Services("").List(v1.ListOptions{})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -193,11 +176,10 @@ func getServices(ch chan Rows, clientset *kubernetes.Clientset) {
 			ips = append(ips, service.Spec.ClusterIP)
 		}
 		rows = append(rows, Row{
-			colorService("[svc]"),
+			colorService("[service]"),
 			colorService(service.ObjectMeta.Namespace),
 			colorService(service.ObjectMeta.Name),
 			colorService(strings.Join(statuses, ",")),
-			colorService(""), // Node
 			colorService(strings.Join(ips, " ") + " " + strings.Join(ports, " ")),
 			colorService(shortHumanDuration(time.Since(service.CreationTimestamp.Time))),
 		})
@@ -206,7 +188,7 @@ func getServices(ch chan Rows, clientset *kubernetes.Clientset) {
 }
 
 func getDeployments(ch chan Rows, clientset *kubernetes.Clientset) {
-	deps, err := clientset.Extensions().Deployments("").List(metav1.ListOptions{})
+	deps, err := clientset.Extensions().Deployments("").List(v1.ListOptions{})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -226,28 +208,16 @@ func getDeployments(ch chan Rows, clientset *kubernetes.Clientset) {
 			}
 			statuses = append(statuses, string(c.Type))
 		}
-		var status string
-		if dep.Status.AvailableReplicas < *dep.Spec.Replicas {
-			status = colorFailed(fmt.Sprintf("%d/%d/%d %s",
-				dep.Status.AvailableReplicas,
-				dep.Status.Replicas,
-				*dep.Spec.Replicas,
-				strings.Join(statuses, " "),
-			))
-		} else {
-			status = colorDeployment(fmt.Sprintf("%d/%d/%d %s",
-				dep.Status.AvailableReplicas,
-				dep.Status.Replicas,
-				*dep.Spec.Replicas,
-				strings.Join(statuses, " "),
-			))
-		}
 		rows = append(rows, Row{
-			colorDeployment("[deploy]"),
+			colorDeployment("[deployment]"),
 			colorDeployment(dep.ObjectMeta.Namespace),
 			colorDeployment(fmt.Sprintf("%v", dep.ObjectMeta.Name)),
-			status,
-			colorDeployment(""), // Node
+			colorDeployment(fmt.Sprintf("DES=%d CUR=%d AVA=%d %s",
+				*dep.Spec.Replicas,
+				dep.Status.Replicas,
+				dep.Status.AvailableReplicas,
+				strings.Join(statuses, " "),
+			)),
 			colorDeployment(""), // IP
 			colorDeployment(shortHumanDuration(time.Since(dep.CreationTimestamp.Time))),
 		})
@@ -255,8 +225,8 @@ func getDeployments(ch chan Rows, clientset *kubernetes.Clientset) {
 	ch <- rows
 }
 
-func getPods(ch chan Rows, clientset *kubernetes.Clientset, lcpNodes string) {
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+func getPods(ch chan Rows, clientset *kubernetes.Clientset) {
+	pods, err := clientset.Core().Pods("").List(v1.ListOptions{})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -269,26 +239,19 @@ func getPods(ch chan Rows, clientset *kubernetes.Clientset, lcpNodes string) {
 		if *flagNamespace != "" && pod.ObjectMeta.Namespace != *flagNamespace {
 			continue
 		}
-		status := string(pod.Status.Phase)
 		var statuses []string
-		statuses = append(statuses, status)
+		statuses = append(statuses, string(pod.Status.Phase))
 		for _, c := range pod.Status.Conditions {
 			if c.Status != "True" {
 				continue
 			}
 			statuses = append(statuses, string(c.Type))
 		}
-		if status == "Running" {
-			status = colorPod(strings.Join(statuses, " "))
-		} else {
-			status = colorFailed(strings.Join(statuses, " "))
-		}
 		rows = append(rows, Row{
 			colorPod("[pod]"),
 			colorPod(pod.ObjectMeta.Namespace),
 			colorPod(fmt.Sprintf("%v", truncate(pod.ObjectMeta.Name))),
-			status,
-			colorPod(strings.TrimPrefix(pod.Spec.NodeName, lcpNodes)), // Node
+			colorPod(strings.Join(statuses, " ")),
 			colorPod(pod.Status.PodIP), //pod.Status.HostIP, pod.ObjectMeta.Labels),
 			colorPod(shortHumanDuration(time.Since(pod.CreationTimestamp.Time))),
 		})
@@ -343,33 +306,6 @@ func shortHumanDuration(d time.Duration) string {
 		return fmt.Sprintf("%dd", hours/24)
 	}
 	return fmt.Sprintf("%dy", int(d.Hours()/24/365))
-}
-
-//  LCP is copied from https://rosettacode.org/wiki/Longest_common_prefix#Go
-func lcp(l []string) string {
-	switch len(l) {
-	case 0:
-		return ""
-	case 1:
-		return l[0]
-	}
-	// LCP of min and max (lexigraphically)
-	// is the LCP of the whole set.
-	min, max := l[0], l[0]
-	for _, s := range l[1:] {
-		switch {
-		case s < min:
-			min = s
-		case s > max:
-			max = s
-		}
-	}
-	for i := 0; i < len(min) && i < len(max); i++ {
-		if min[i] != max[i] {
-			return min[:i]
-		}
-	}
-	return min
 }
 
 func clear() {
